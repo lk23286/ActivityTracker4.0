cdf31312950c3231a77a8bfce915df603e2c7d0b
lk23286 <lk23286@gmail.com>
2023-06-14 19:02:00 +0200

    add ArcThemeColor struct in Models folder

---
+++ ActivityTracker4.0/Models/ArchThemeColor.swift
@@ -0,0 +1,98 @@
+//
+//  ArchThemeColor.swift
+//  ActivityTracker4.0
+//
+//  Created by Laszlo Kovacs on 2023. 06. 14..
+//
+
+import Foundation
+import SwiftUI
+
+enum ArcThemeColor: String, CaseIterable, Identifiable {
+    
+    case lightGreenInk
+    case lightBlueInk
+    case lightYellowInk
+    case lightGrayInk
+    
+    var inkColor: Color {
+        Color(rawValue)
+    }
+    
+    var paperColor: Color {
+        let color:Color
+        switch self {
+        case .lightGreenInk :
+            color = ThemeColor.lightGreenPaper.color
+        case .lightBlueInk :
+            color = ThemeColor.lightBluePaper.color
+        case .lightYellowInk :
+            color = ThemeColor.lightYellowPaper.color
+        case .lightGrayInk :
+            color = ThemeColor.lightGrayPaper.color
+        }
+        return color
+    }
+    
+    var arcNumber: Int {
+        let number: Int
+        switch self {
+        case .lightGreenInk :
+            number = 1
+        case .lightBlueInk :
+            number = 2
+        case .lightYellowInk :
+            number = 3
+        case .lightGrayInk :
+            number = 4
+        }
+        return number
+        
+    }
+
+    var name: String {
+        rawValue
+        
+    }
+    var id: String {
+        name
+    }
+}
+
+enum ThemeColor: String, CaseIterable, Identifiable {
+    
+    case lightGreenInk
+    case lightGreenPaper
+    case lightBlueInk
+    case lightBluePaper
+    case lightYellowInk
+    case lightYellowPaper
+    case lightGrayInk
+    case lightGrayPaper
+    
+    case  darkGreenInk
+    case  darkGreenPaper
+    case  darkBlueInk
+    case  darkBluePaper
+    case  darkYellowInk
+    case  darkYellowPaper
+    case  darkGrayInk
+    case  darkGrayPaper
+    
+    var color: Color {
+        Color(rawValue)
+    }
+    
+    var name: String {
+        rawValue
+    }
+    var id: String {
+        name
+    }
+}
+
+struct K {
+    static var backgroundGray = "bkgGray"
+}
+
+
---
+++ ActivityTracker4.0/Models/DiagramArc.swift
@@ -0,0 +1,34 @@
+//
+//  DiagramArc.swift
+//  ActivityTracker4.0
+//
+//  Created by Laszlo Kovacs on 2023. 06. 14..
+//
+
+import Foundation
+import SwiftUI
+
+struct DiagramArc: Shape {
+    
+    let number: Int
+    let percent: Int
+    var diagramRadius = 0.0
+    var arcWidth: Int = 1
+    
+    private var startAngle: Angle { Angle(degrees: 0.0) }
+    private var endAngle: Angle { Angle(degrees: Double(percent) * 3.6) }
+    
+    private var diameterModifier: Double {
+        1.25 - Double(number) * 0.25
+    }
+    
+    
+    func path(in rect: CGRect) -> Path {
+        let diameter = min(rect.size.width, rect.size.height) - 35
+        let radius = ( diameter * diameterModifier) / 2
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        return Path { path in
+            path.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+        }
+    }
+}
