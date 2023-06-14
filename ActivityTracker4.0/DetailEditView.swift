ba0876d21319ff7b54e1ee043135a9e3346b0da6
lk23286 <lk23286@gmail.com>
2023-06-14 19:20:52 +0200

    add DetailEditView

---
+++ ActivityTracker4.0/DetailEditView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailEditView.swift
+//  ActivityTracker4.0
+//
+//  Created by Laszlo Kovacs on 2023. 06. 14..
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
