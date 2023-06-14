6397df98aa0bfbe1c592beca457620b2c4f803e8
lk23286 <lk23286@gmail.com>
2023-06-14 19:24:56 +0200

    add CounterView

---
+++ ActivityTracker4.0/CounterView.swift
@@ -0,0 +1,54 @@
+//
+//  CounterView.swift
+//  ActivityTracker4.0
+//
+//  Created by Laszlo Kovacs on 2023. 06. 14..
+//
+
+import SwiftUI
+
+struct CounterView: View {
+    
+    @Binding var progress: Double
+  
+    
+
+    var body: some View {
+        
+        Form {
+            Section(header: Text("Progress:")) {
+                
+//                HStack {
+//                    Slider(value: $progress, in: 1...200, step: 1, label: {
+//                        Text("Duration")
+//                            .padding()
+//                    })
+//
+//                    Text("\(Int(progress)) min")
+//                }
+                Text("Counter: 00:00:00")
+                    .font(.largeTitle)
+                
+                
+            }
+
+                
+                    .navigationTitle("Counter")
+           
+        }
+        
+
+    }
+}
+
+struct CounterView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        NavigationView {
+            CounterView(progress: .constant(Activity.lightSample[0].progress))
+        }
+        
+        
+      
+    }
+}
