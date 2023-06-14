06a92e4d635b749c8424aa5183646441a41280d1
lk23286 <lk23286@gmail.com>
2023-06-14 18:45:17 +0200

    Initial Commit

---
+++ ActivityTracker4.0/ActivityTracker4_0App.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTracker4_0App.swift
+//  ActivityTracker4.0
+//
+//  Created by Laszlo Kovacs on 2023. 06. 14..
+//
+
+import SwiftUI
+
+@main
+struct ActivityTracker4_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActivityTracker4.0/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  ActivityTracker4.0
+//
+//  Created by Laszlo Kovacs on 2023. 06. 14..
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
