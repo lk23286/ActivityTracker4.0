4504a83e6760b42ee27ef979d8962c05a362452f
lk23286 <lk23286@gmail.com>
2023-06-14 19:18:56 +0200

    add ThemePicker view

---
+++ ActivityTracker4.0/ThemePicker.swift
@@ -0,0 +1,30 @@
+//
+//  ThemePicker.swift
+//  ActivityTracker4.0
+//
+//  Created by Laszlo Kovacs on 2023. 06. 14..
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: ArcThemeColor
+    
+    var body: some View {
+        
+        Picker("", selection: $selection) {
+            ForEach(ArcThemeColor.allCases) { arcThemeColor in
+                
+                ThemeView(arcThemeColor: arcThemeColor)
+                    .tag(arcThemeColor)
+            }
+        }
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        ThemePicker(selection: .constant(ArcThemeColor.lightGreenInk) )
+    }
+}
