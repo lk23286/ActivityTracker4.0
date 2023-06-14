3d858ce379001d2a5f7cfa709d0b3f01e3a367e2
lk23286 <lk23286@gmail.com>
2023-06-14 19:05:50 +0200

    add errorWrapper struct in Models folder

---
+++ ActivityTracker4.0/Models/ErrorWrapper.swift
@@ -0,0 +1,32 @@
+//
+//  ErrorWrapper.swift
+//  ActivityTracker4.0
+//
+//  Created by Laszlo Kovacs on 2023. 06. 14..
+//
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+    
+    init(id: UUID = UUID(), error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+   
+}
+
+enum ActivityErrors: Error {
+    case reachedMaxActivities
+}
+
+extension ErrorWrapper {
+    static let basicErrors: [ErrorWrapper] = [
+        ErrorWrapper(error: ActivityErrors.reachedMaxActivities, guidance: "Can't add more activities")]
+    
+}
+
