87c490cfd5429b90b4715c2fe13ce748519cee56
lk23286 <lk23286@gmail.com>
2023-06-14 19:28:39 +0200

    add MainDiagramView

---
+++ ActivityTracker4.0/MainDiagramView.swift
@@ -0,0 +1,38 @@
+//
+//  MainDiagramView.swift
+//  ActivityTracker4.0
+//
+//  Created by Laszlo Kovacs on 2023. 06. 14..
+//
+
+import SwiftUI
+
+struct MainDiagramView: View {
+    
+    var activities: [Activity]
+    
+    var body: some View {
+        Circle()
+            .strokeBorder(lineWidth: 0)
+            .background(Color(K.backgroundGray)
+            )
+            .overlay {
+
+                ForEach(activities) { activity in
+                        ArcView(achivement: activity.achievement, archThemeColor: activity.arcThemeColor)
+                }
+ 
+            }
+            .background(Color(K.backgroundGray))
+    }
+}
+
+struct MainDiagramView_Previews: PreviewProvider {
+    
+   // static var activities: [Activity] = Activity.lightSample
+  
+    
+    static var previews: some View {
+        MainDiagramView(activities: Activity.lightSample)
+    }
+}
