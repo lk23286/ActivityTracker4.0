603ae06d1b92fe621aa7501d79e5274e265562d2
lk23286 <lk23286@gmail.com>
2023-06-14 19:27:15 +0200

    add MainHeaderView

---
+++ ActivityTracker4.0/MainHeaderView.swift
@@ -0,0 +1,36 @@
+//
+//  MainHeaderView.swift
+//  ActivityTracker4.0
+//
+//  Created by Laszlo Kovacs on 2023. 06. 14..
+//
+
+
+import SwiftUI
+
+struct MainHeaderView: View {
+    
+  @Binding var activity: Activity
+    
+    
+    var body: some View {
+        
+            Text(activity.name)
+                .font(.title2)
+                .foregroundColor(activity.arcThemeColor.inkColor)
+                .fontWeight(.bold)
+
+    }
+    
+}
+
+struct MainHeaderView_Previews: PreviewProvider {
+    
+     
+    static var previews: some View {
+       
+        MainHeaderView(activity: .constant(Activity.lightSample[0]))
+      
+    }
+}
+
